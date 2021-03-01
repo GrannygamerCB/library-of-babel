53a45c4c32ba7639325ca10e2875c6a9039e8b2f
annabellepress <72040308+annabellepress@users.noreply.github.com>
2021-02-28 20:33:30 -0500

    Add files via upload

---
+++ paragraphs/assets/script.js
@@ -0,0 +1,526 @@
+//full text//
+$(function() {
+  $('#title').hover(function() {
+    $('#summary').css('color', '#ff5500');
+  }, function() {
+    $('#summary').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#name').hover(function() {
+    $('#about').css('color', '#ff5500');
+    $('#projectA').css('color', '#ff5500')
+    $('#projectB').css('color', '#ff5500')
+    $('#projectC').css('color', '#ff5500')
+    $('#projectD').css('color', '#ff5500')
+    $('#projectE').css('color', '#ff5500')
+  }, function() {
+    $('#about').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+    $('#projectA').css('color', '#9C9C9C', 'transition', '0.4s all ease')
+    $('#projectB').css('color', '#9C9C9C', 'transition', '0.4s all ease')
+    $('#projectC').css('color', '#9C9C9C', 'transition', '0.4s all ease')
+    $('#projectD').css('color', '#9C9C9C', 'transition', '0.4s all ease')
+    $('#projectE').css('color', '#9C9C9C', 'transition', '0.4s all ease')
+  });
+ });
+
+//vocab//
+
+/*p1*/
+/*p2*/
+$(function() {
+  $('#universe').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#indefinite').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#infinite').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#interminably').hover(function() {
+    $('#vocabD').css('color', '#ff5500');
+  }, function() {
+    $('#vocabD').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#invariable').hover(function() {
+    $('#vocabE').css('color', '#ff5500');
+  }, function() {
+    $('#vocabE').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#abysmally').hover(function() {
+    $('#vocabF').css('color', '#ff5500');
+  }, function() {
+    $('#vocabF').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#faithfully').hover(function() {
+    $('#vocabG').css('color', '#ff5500');
+  }, function() {
+    $('#vocabG').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#incessant').hover(function() {
+    $('#vocabH').css('color', '#ff5500');
+  }, function() {
+    $('#vocabH').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+/*p3*/
+$(function() {
+  $('#fathomless').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+$(function() {
+  $('#pious').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ });
+
+/*p4*/
+$(function() {
+  $('#axioms').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p5*/
+$(function() {
+  $('#ab-aeterno').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#demiurgi').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#enigmatical').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p6*/
+$(function() {
+  $('#orthographical').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#labyrinth').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#cacophonies').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#uncouth').hover(function() {
+    $('#vocabD').css('color', '#ff5500');
+  }, function() {
+    $('#vocabD').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#fallacious').hover(function() {
+    $('#vocabE').css('color', '#ff5500');
+  }, function() {
+    $('#vocabE').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p7*/
+$(function() {
+  $('#dialectical').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#cryptographs').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p8*/
+$(function() {
+  $('#yiddish').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#samoyedic').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#guarani').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#incontrovertible').hover(function() {
+    $('#vocabD').css('color', '#ff5500');
+  }, function() {
+    $('#vocabD').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#gnostic').hover(function() {
+    $('#vocabE').css('color', '#ff5500');
+  }, function() {
+    $('#vocabE').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#gospel').hover(function() {
+    $('#vocabF').css('color', '#ff5500');
+  }, function() {
+    $('#vocabF').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p9*/
+$(function() {
+  $('#vindications').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#prodigious').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#arcana').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p10*/
+$(function() {
+  $('#verisimilar').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+/*p11*/
+$(function() {
+  $('#blasphemous').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#canonical').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p12*/
+$(function() {
+  $('#condemned').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#ascetic').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#perdition').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#execrated').hover(function() {
+    $('#vocabD').css('color', '#ff5500');
+  }, function() {
+    $('#vocabD').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#infinitesimal').hover(function() {
+    $('#vocabE').css('color', '#ff5500');
+  }, function() {
+    $('#vocabE').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#facsimiles').hover(function() {
+    $('#vocabF').css('color', '#ff5500');
+  }, function() {
+    $('#vocabF').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+/*p13*/
+$(function() {
+  $('#superstition').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#compendium').hover(function() {
+    $('#vocabF').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#analogous').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#venerated').hover(function() {
+    $('#vocabD').css('color', '#ff5500');
+  }, function() {
+    $('#vocabD').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#impious').hover(function() {
+    $('#vocabE').css('color', '#ff5500');
+  }, function() {
+    $('#vocabE').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#allegorical').hover(function() {
+    $('#vocabF').css('color', '#ff5500');
+  }, function() {
+    $('#vocabF').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#tautology').hover(function() {
+    $('#vocabG').css('color', '#ff5500');
+  }, function() {
+    $('#vocabG').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#ubiquitous').hover(function() {
+    $('#vocabH').css('color', '#ff5500');
+  }, function() {
+    $('#vocabH').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p14*/
+$(function() {
+  $('#phantoms').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#epidemics').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#heretical').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#peregrinations').hover(function() {
+    $('#vocabD').css('color', '#ff5500');
+  }, function() {
+    $('#vocabD').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+/*p15*/
+$(function() {
+  $('#rhetorical').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#cyclical').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+/*p16*/
+$(function() {
+  $('#pulmonary').hover(function() {
+    $('#vocabA').css('color', '#ff5500');
+  }, function() {
+    $('#vocabA').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#superimposition').hover(function() {
+    $('#vocabB').css('color', '#ff5500');
+  }, function() {
+    $('#vocabB').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+ $(function() {
+  $('#vade-mecum').hover(function() {
+    $('#vocabC').css('color', '#ff5500');
+  }, function() {
+    $('#vocabC').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+//notes & commentary//
+$(function() {
+  $('#commentA').hover(function() {
+    $('#commentAc').css('color', '#ff5500');
+  }, function() {
+    $('#commentAc').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#commentB').hover(function() {
+    $('#commentBc').css('color', '#ff5500');
+  }, function() {
+    $('#commentBc').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+$(function() {
+  $('#commentC').hover(function() {
+    $('#commentCc').css('color', '#ff5500');
+  }, function() {
+    $('#commentCc').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+$(function() {
+  $('#commentD').hover(function() {
+    $('#commentDc').css('color', '#ff5500');
+  }, function() {
+    $('#commentDc').css('color', '#9C9C9C', 'transition', '0.4s all ease');
+  });
+ })
+
+
+
+
+
